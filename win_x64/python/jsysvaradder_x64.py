a7c28adaf0842ba0d3f7c34c615633b94b48c7dc
DonggeonMoon <83414846+DonggeonMoon@users.noreply.github.com>
2021-06-15 16:36:16 +0900

    Update jsysvaradder_x64.py

--- win_x64/python/jsysvaradder_x64.py
+++ win_x64/python/jsysvaradder_x64.py
@@ -1,6 +1,6 @@
 #이 프로그램은 관리자 권한으로 실행돼야 합니다. This program MUST be run as administrator.
 import os
 
-version = str(input("자바 버전을 입력하세요"))
+version = str(input("JDK 버전을 입력하세요"))
 os.system("setx JAVA_HOME -m \"C:\\Program Files\\Java\\jdk-"+version+"\"")
 os.system("setx Path -m \"%Path%;%JAVA_HOME%\\bin\"")
